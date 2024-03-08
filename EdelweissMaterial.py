ffd292b596180d72a453888abdcf7852b187e5d7
danielrtm00 <148960323+danielrtm00@users.noreply.github.com>
2024-03-08 16:02:08 +0100

    Add files via upload

--- EdelweissMaterial.py
+++ EdelweissMaterial.py
@@ -26,58 +26,13 @@ class EdelweissMaterial:
         else:
             raise Exception("This material type doesn't exist (yet). Current material: " + self.materialname)
 
-    def computeElasticity(self, dim: int, plStrain: bool):
-        """Compute the elasticity matrix for the material depending on the element. 
-        This method is only used for the calculation of the stiffness matrix.
-
-        Parameters
-        ----------
-        elementType
-            A string identifying the requested element formulation as shown in element."""
-
-        if dim == 2 and plStrain:
-            # Ei = elasticity matrix for plane strain for element i (mostly the case for rectangular elements)
-            Ei = (
-                (self.E * (1 - self.v))
-                / ((1 + self.v) * (1 - 2 * self.v))
-                * np.array(
-                    [
-                        [1, self.v / (1 - self.v), 0],
-                        [self.v / (1 - self.v), 1, 0],
-                        [0, 0, (1 - 2 * self.v) / (2 * (1 - self.v))],
-                    ]
-                )
-            )
-        elif dim == 2 and not plStrain:
-            # Ei = elasticity matrix for plane stress for element i
-            Ei = self.E / (1 - self.v**2) * np.array([[1, self.v, 0], [self.v, 1, 0], [0, 0, (1 - self.v) / 2]])
-        elif dim == 3:
-            # 3D elasticity matrix for Hexa8
-            Ei = (
-                self.E
-                / ((1 + self.v) * (1 - 2 * self.v))
-                * np.array(
-                    [
-                        [(1 - self.v), self.v, self.v, 0, 0, 0],
-                        [self.v, (1 - self.v), self.v, 0, 0, 0],
-                        [self.v, self.v, (1 - self.v), 0, 0, 0],
-                        [0, 0, 0, (1 - 2 * self.v) / 2, 0, 0],
-                        [0, 0, 0, 0, (1 - 2 * self.v) / 2, 0],
-                        [0, 0, 0, 0, 0, (1 - 2 * self.v) / 2],
-                    ]
-                )
-            )
-        return Ei
-
     def computeStress(
         self,
         stress: np.ndarray,
         dStressdStrain: np.ndarray,
-        strain: np.ndarray,
+        dStrain: np.ndarray,
         time: float,
         dTime: float,
-        dU: np.ndarray,
-        U: np.ndarray,
         dim: int,
         plStrain: bool,
     ):
@@ -86,19 +41,30 @@ class EdelweissMaterial:
         Parameters
         ----------
         stress
-            Matrix containing the stresses in the element.
+            Vector containing the stresses in the element.
         dStressdStrain
             Matrix containing dStress/dStrain.
-        strain
-            Strain matrix at time t = n before and t = n+1 after this function.
+        dStrain
+            Strain vector increment at time step t to t+dTime.
         time
-            New time after this step.
+            Array of step time and total time.
         dTime
             Current time step size.
-        dU
-            Change in displacement in this time step.
+        dim
+            Dimension of the model (2D or 3D).
+        plStrain
+            Checks if the elasticity matrix should be plane strain or plane stress.
         """
 
+        try:
+            self.strain
+        except AttributeError:
+            if dim == 2:
+                self.strain = np.zeros([3])
+                self.strainOld = np.zeros([3])
+            elif dim == 3:
+                self.strain = np.zeros([6])
+                self.strainOld = np.zeros([6])
         if self.materialname.lower() == "linearelastic":
             if dim == 2 and plStrain:
                 # Ei = elasticity matrix for plane strain for element i (mostly the case for rectangular elements)
@@ -132,11 +98,27 @@ class EdelweissMaterial:
                         ]
                     )
                 )
+            # calculate new strain
+            if time[-1] == 0:  # first step
+                self.strain = dStrain  # reset to first value
+                self.strainOld = dStrain  # = also old value
+            else:
+                try:
+                    self._time
+                except AttributeError:
+                    self._time = time[-1] # save old time
+                if self._time == time[-1]:  # total time stayed the same, step failed
+                    self.strain = self.strainOld + dStrain  # reset to old value and add step
+                elif self._time != time[-1]:  # total time changed, step successful
+                    self.strainOld = self.strain # set old value to successful step
+                    self.strain += dStrain  # add to get new value
+                    self._time = time[-1] # total time now
+            #pdb.set_trace()
             # calculate new stress with new strain
-            stress = Ei @ strain
+            self.stress = Ei @ self.strain[..., None]
             # for linear elastic it's just the elasticity matrix
-            dStressdStrain = Ei
+            self.dStressdStrain = Ei
         else:
-            raise Exception("This material type doesn't exist (yet). Current material: " + self.materialname)
+            raise Exception("This material type doesn't exist (yet). Selected material: " + self.materialname)
         # if one needs the direct output:
-        return {"stress": stress, "strain": strain, "dStressdStrain": dStressdStrain}
+        # return {"stress": stress, "strain": dStrain, "dStressdStrain": dStressdStrain}
